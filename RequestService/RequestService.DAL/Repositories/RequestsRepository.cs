a270f1c2a71393571a1af7035f6bad6f5c245afe
DjPetuhe <validhernufKPI@gmail.com>
2023-03-07 02:34:34 +0200

    Hardcoded database for first lab

--- RequestService/RequestService.DAL/Repositories/RequestsRepository.cs
+++ RequestService/RequestService.DAL/Repositories/RequestsRepository.cs
@@ -8,41 +8,57 @@ namespace RequestService.DAL.Repositories
     public class RequestsRepository : IRequestsRepository
     {
         private readonly DataContext _context;
-        
+
+        private static List<Request> requests = new() //TEMP : HARDCODE
+        {
+            new() { Id = 1, UserId = "userID", AdvertId = 1, Status = Enums.RequestStatusEnum.applied },
+            new() { Id = 2, UserId = "userID", AdvertId = 2, Status = Enums.RequestStatusEnum.applied },
+            new() { Id = 3, UserId = "userID", AdvertId = 1, Status = Enums.RequestStatusEnum.applied },
+            new() { Id = 4, UserId = "userID", AdvertId = 5, Status = Enums.RequestStatusEnum.applied },
+            new() { Id = 5, UserId = "userID", AdvertId = 4, Status = Enums.RequestStatusEnum.applied },
+            new() { Id = 6, UserId = "userID", AdvertId = 5, Status = Enums.RequestStatusEnum.applied },
+        };
+
         public RequestsRepository(DataContext context)
         {
             _context = context;
         }
 
-        public Task<List<Request>> GetAll()
+        public async Task<List<Request>> GetAll() //TEMP: ASYNC FOR HARDCODE
         {
-            return _context.Requests.ToListAsync();
+            return requests; //TEMP: HARDCODE
+            //return _context.Requests.ToListAsync();
         }
 
-        public Task<Request?> GetById(int id)
+        public async Task<Request?> GetById(int id) //TEMP: ASYNC FOR HARDCODE
         {
-            return _context.Requests.FindAsync(id).AsTask();
+            return requests.Where(r => r.Id == id).FirstOrDefault(); //TEMP: HARDCODE
+            //return _context.Requests.FindAsync(id).AsTask();
         }
 
-        public Task<List<Request>> GetByAdvertId(int advertId)
+        public async Task<List<Request>> GetByAdvertId(int advertId) //TEMP: ASYNC FOR HARDCODE
         {
-            return _context.Requests.Where(r => advertId == r.AdvertId).ToListAsync();
+            return requests.Where(r => r.AdvertId == advertId).ToList(); //TEMP: HARDCODE
+            //return _context.Requests.Where(r => advertId == r.AdvertId).ToListAsync();
         }
 
-        public async Task Add(Request requestToAdd)
+        public async Task Add(Request requestToAdd) //TEMP: ASYNC FOR HARDCODE
         {
-            await _context.Requests.AddAsync(requestToAdd);
+            requests.Add(requestToAdd);
+            //await _context.Requests.AddAsync(requestToAdd);
         }
         
         public void Update(Request requestToUpdate)
         {
-            _context.Set<Request>().Attach(requestToUpdate);
-            _context.Entry(requestToUpdate).State = EntityState.Modified;
+            requests.Where(r => r.Id == requestToUpdate.Id).ToList().ForEach(s => s.Status = requestToUpdate.Status); //TEMP: HARDCODE
+            //_context.Set<Request>().Attach(requestToUpdate);
+            //_context.Entry(requestToUpdate).State = EntityState.Modified;
         }
 
         public void Delete(Request requestToRemove)
         {
-            _context.Requests.Remove(requestToRemove);
+            requests.Remove(requestToRemove);
+            //_context.Requests.Remove(requestToRemove);
         }
 
         public Task<int> SaveChangesAsync()
