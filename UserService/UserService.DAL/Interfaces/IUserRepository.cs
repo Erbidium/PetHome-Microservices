3b06071f5d0b2ee28c046e03a6e8f6ca3a8b8979
Oleksandr <OlexandrTanasienko@gmail.com>
2023-03-09 01:37:23 +0200

    Create interface for user repository

---
+++ UserService/UserService.DAL/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using UserService.DAL.Entities;
+
+namespace UserService.DAL.Interfaces;
+
+public interface IUserRepository
+{
+    Task<User?> GetById(long id);
+    Task<List<User>> Get();
+    Task Add(User entity);
+    void Update(User advertToUpdate);
+    void Delete(User entity);
+    Task<int> SaveChangesAsync();
+}
