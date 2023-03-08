62ccea2519b1f2cb872c66cbfe7ceb2c6503d844
Oleksandr <OlexandrTanasienko@gmail.com>
2023-03-09 01:33:14 +0200

    Add automapper user profile

---
+++ UserService/UserService.BLL/MappingProfiles/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using UserService.BLL.DTOs;
+using UserService.DAL.Entities;
+
+namespace UserService.BLL.MappingProfiles;
+
+public class UserProfile : Profile
+{
+    public UserProfile()
+    {
+        CreateMap<User, UserDto>();
+        CreateMap<UserDto, User>();
+    }
+}
