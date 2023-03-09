51da699c8da07ac98ea00246325bf2336fc9f833
Oleksandr <OlexandrTanasienko@gmail.com>
2023-03-09 22:52:03 +0200

    Update mocked users data

--- UserService/UserService.DAL/Repositories/MockUserRepository.cs
+++ UserService/UserService.DAL/Repositories/MockUserRepository.cs
@@ -7,11 +7,11 @@ public class MockUserRepository : IUserRepository
 {
     private static readonly List<User> Users = new()
     {
-        new User {Id = 1, Name = "name1", Location = "location1", Email = "email1@email.com", Password = "hashedPassword1" },
-        new User {Id = 2, Name = "name2", Location = "location2", Email = "email2@email.com", Password = "hashedPassword2" },
-        new User {Id = 3, Name = "name3", Location = "location3", Email = "email3@email.com", Password = "hashedPassword3" },
-        new User {Id = 4, Name = "name4", Location = "location4", Email = "email4@email.com", Password = "hashedPassword4" },
-        new User {Id = 5, Name = "name5", Location = "location5", Email = "email5@email.com", Password = "hashedPassword5" }
+        new User {Id = 1, Name = "Петро", Location = "Київ", Email = "email1@email.com", Password = "hashedPassword1" },
+        new User {Id = 2, Name = "Максим", Location = "Львів", Email = "email2@email.com", Password = "hashedPassword2" },
+        new User {Id = 3, Name = "Данило", Location = "Полтава", Email = "email3@email.com", Password = "hashedPassword3" },
+        new User {Id = 4, Name = "Богдан", Location = "Вінниця", Email = "email4@email.com", Password = "hashedPassword4" },
+        new User {Id = 5, Name = "Віталій", Location = "Харків", Email = "email5@email.com", Password = "hashedPassword5" }
 
     };
     
