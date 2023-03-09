68dc4df53991085e2b36e997d231f335d19bf6fe
DjPetuhe <validhernufKPI@gmail.com>
2023-03-09 21:04:58 +0200

    Load requests data and add spinner

--- pethome-client/src/Pages/Requests/Requests.jsx
+++ pethome-client/src/Pages/Requests/Requests.jsx
@@ -1,3 +1,30 @@
+import {useEffect, useState} from "react";
+import {useFetching} from "../../Hooks/useFetching";
+import RequestService from "../../HTTP/RequestService";
+import {MyLoader} from "../../UI/MyLoader/MyLoader";
+
 export default function Requests() {
-    return <div>Requests</div>
+    const [requests, setRequests] = useState([]);
+    const [updateRequests, setUpdateRequests] = useState(false);
+    const [prevData, setPrevData] = useState({});
+    const [isUpdate, setIsUpdate] = useState(false);
+
+    const [fetchRequests, loader, error] = useFetching(async () => {
+        const response = await RequestService.getAllRequests()
+        setRequests(response.data)
+    })
+
+    useEffect(() => {
+        async function fetch() {
+            try {
+                await fetchRequests()
+            } catch (e) {
+                console.error(error)
+            }
+        }
+        fetch()
+    }, [updateRequests])
+
+    if (loader) return <MyLoader />
+    return <div>Users</div>
 }
