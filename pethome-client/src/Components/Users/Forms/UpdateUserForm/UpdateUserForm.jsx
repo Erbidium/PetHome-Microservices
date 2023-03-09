874297a56b0832b54dce8bbb966011806d4caead
Oleksandr <OlexandrTanasienko@gmail.com>
2023-03-09 22:05:25 +0200

    Create separate component for user update

--- pethome-client/src/Components/Users/Forms/UpdateUserForm/UpdateUserForm.jsx
+++ pethome-client/src/Components/Users/Forms/UpdateUserForm/UpdateUserForm.jsx
@@ -1,28 +1,26 @@
 import React, { useState,useEffect } from 'react'
 import { useFetching } from '../../../../Hooks/useFetching'
-import s from './CreateUserForm.module.css'
+import s from './UpdateUserForm.module.css'
 import { MyLoader } from '../../../../UI/MyLoader/MyLoader'
 import UserService from "../../../../HTTP/UserService";
 
-export const CreteUserForm = ({ updateUsers, setUpdateUsers, prevData }) => {
+export const UpdateUserForm = ({ updateUsers, setUpdateUsers, prevData }) => {
 
     const [userData, setUserData] = useState(prevData);
 
-    const [createUserFetching, loader, error] = useFetching(async () => {
+    const [updateUserFetching, loader2, error2] = useFetching(async () => {
         const formData = new FormData();
         Object.keys(userData).forEach(function (key, index) {
             formData.append(key, Object.values(userData)[index])
         })
-        await UserService.createUser(formData)
+        await UserService.updateUser(formData, prevData?.id)
     })
-
-    async function createUser(e) {
+    async function updateUser(e) {
         e.preventDefault()
-        console.log(userData)
         try {
-            await createUserFetching()
+            await updateUserFetching()
         } catch (e) {
-            console.error(error)
+            console.error(error2)
         }
         setUserData({ name: '', description: '', startTime: '', endTime: '', location: '', cost: '' })
         setUpdateUsers(!updateUsers)
@@ -32,6 +30,6 @@ export const CreteUserForm = ({ updateUsers, setUpdateUsers, prevData }) => {
         setUserData(prevData)
     }, [prevData]);
 
-    if (loader) return <MyLoader />
+    if (loader2) return <MyLoader />
 
 }
