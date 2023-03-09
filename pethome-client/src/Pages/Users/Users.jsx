c5e7962182ae651d8882ce427157422cf7c30a7c
Oleksandr <OlexandrTanasienko@gmail.com>
2023-03-09 20:30:30 +0200

    Load users data and show spinner

--- pethome-client/src/Pages/Users/Users.jsx
+++ pethome-client/src/Pages/Users/Users.jsx
@@ -1,3 +1,31 @@
+import {useEffect, useState} from "react";
+import {useFetching} from "../../Hooks/useFetching";
+import AdvertService from "../../HTTP/AdvertService";
+import UserService from "../../HTTP/UserService";
+import {MyLoader} from "../../UI/MyLoader/MyLoader";
+
 export default function Users() {
+    const [users, setUsers] = useState([]);
+    const [updateUsers, setUpdateUsers] = useState(false);
+    const [prevData, setPrevData] = useState({});
+    const [isUpdate, setIsUpdate] = useState(false);
+
+    const [fetchUsers, loader, error] = useFetching(async () => {
+        const response = await UserService.getAllUsers()
+        setUsers(response.data)
+    })
+
+    useEffect(() => {
+        async function fetch() {
+            try {
+                await fetchUsers()
+            } catch (e) {
+                console.error(error)
+            }
+        }
+        fetch()
+    }, [updateUsers])
+
+    if (loader) return <MyLoader />
     return <div>Users</div>
 }
