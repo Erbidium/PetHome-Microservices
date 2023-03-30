f00c13a40c430b2b344b9847cbce7aa1dcb13ced
Oleksandr <OlexandrTanasienko@gmail.com>
2023-03-30 11:00:17 +0300

    Add ef context

--- UserService/UserService.DAL/Entities/User.cs
+++ UserService/UserService.DAL/Entities/User.cs
@@ -1,7 +1,12 @@
-﻿namespace UserService.DAL.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UserService.DAL.Entities;
 
 public class User
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public Guid Id { get; set; }
     public string Email { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
