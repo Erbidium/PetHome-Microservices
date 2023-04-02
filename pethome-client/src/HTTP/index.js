4c9bd8ce6f7ca403c09111ae37d4b7a82e6f3500
Oleksandr <OlexandrTanasienko@gmail.com>
2023-04-02 03:12:30 +0300

    Use alpine, create workflow for images

--- pethome-client/src/HTTP/index.js
+++ pethome-client/src/HTTP/index.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
-export const url = 'http://172.30.135.173' // 'http://172.24.79.8'
 
 const $api = axios.create({
     withCredentials: true,
-    baseURL: url,
+    baseURL: '/',
 })
 
 export default $api;
