a57e00f6ea657dbb7fef17ac0d8fe3c3137423bc
DjPetuhe <validhernufKPI@gmail.com>
2023-04-30 23:07:00 +0300

    script to localhost

--- test/main.js
+++ test/main.js
@@ -1,9 +1,4 @@
 import axios from "axios";
-import exec from 'child_process'; 
-
-function execute(command, callback){
-    exec(command, function(error, stdout, stderr){ callback(stdout); });
-};
 
 async function sendRequestsTest(route, count) {
     let failed = 0;
@@ -22,11 +17,9 @@ async function sendRequestsTest(route, count) {
     console.log(`${failed} from ${count} failed\n`);
 }
 
-execute('minikube service request-service --url', async function(ip) {
-    const testAmount = 5;
-    const requestsAmount = 100;
-    const route = ip.substring(0, ip.length - 1) + "/api/requests/with-advert/1"; 
-    for (let i = 0; i < testAmount; i++) {
-        await sendRequestsTest(route, requestsAmount);
-    }
-})  
+const testAmount = 5;
+const requestsAmount = 100;
+const route = "http://loclahost/api/requests/with-advert/1"; 
+for (let i = 0; i < testAmount; i++) {
+    await sendRequestsTest(route, requestsAmount);
+} 
