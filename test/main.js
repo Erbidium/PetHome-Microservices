7d2b7aa625ce7d7d40b4d9b719dcc9f46d89fa7c
Oleksandr <OlexandrTanasienko@gmail.com>
2023-04-30 19:29:26 +0300

    Fix test script

--- test/main.js
+++ test/main.js
@@ -1,19 +1,21 @@
 import axios from "axios";
+import * as child_process from 'child_process'; 
 
 function execute(command, callback){
-    exec(command, function(error, stdout, stderr){ callback(stdout); });
+    child_process.exec(command, function(error, stdout, stderr){ callback(stdout); });
 };
 
 async function sendRequestsTest(route, count) {
     let failed = 0;
     let time = 0;
+    console.log(route);
     for (let i = 0; i < count; i++) {
         const start = performance.now();
         try {
             await axios.get(route);
         } catch (error) {
             failed++;
-            console.log(`ERROR: attempt${i} status:${err.response?.status}`)
+            console.log(`ERROR: attempt${i} status:${error.response?.status}`)
         }
         time += Number((performance.now() - start) / 1000);
     }
@@ -24,7 +26,7 @@ async function sendRequestsTest(route, count) {
 const testAmount = 5;
 const requestsAmount = 100;
 execute('minikube ip', async function(ip){
-    route = "http://" + ip + "/api/requests/with-advert/1"; 
+    let route = "http://localhost/api/requests/with-advert/2"; 
     for (let i = 0; i < testAmount; i++) {
         await sendRequestsTest(route, requestsAmount);
     }
